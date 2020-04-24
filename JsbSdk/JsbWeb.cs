14b424441b1f3c35d87ed2b5562fd0dcf8cf3845
Jingchao Feng <fjch1997@outlook.com>
2020-04-24 00:06:52 -0400

    netstandard

--- JsbSdk/JsbWeb.cs
+++ JsbSdk/JsbWeb.cs
@@ -1,14 +1,11 @@
-﻿using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Digests;
+﻿using Org.BouncyCastle.Crypto.Digests;
 using Org.BouncyCastle.Crypto.Macs;
 using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Utilities.Encoders;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
